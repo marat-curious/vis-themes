03754ea1c9fc8957d5f180b20d2b0a3de57dc9ba
Marat <marat.curious@gmail.com>
2024-11-05 09:56:09 +0300

    create theme

---
+++ themes/plain.lua
@@ -0,0 +1,49 @@
+local lexers = vis.lexers
+
+lexers.STYLE_DEFAULT = ''
+lexers.STYLE_NOTHING = ''
+lexers.STYLE_ATTRIBUTE = ''
+lexers.STYLE_CLASS = ''
+lexers.STYLE_COMMENT = ''
+lexers.STYLE_CONSTANT = ''
+lexers.STYLE_DEFINITION = ''
+lexers.STYLE_ERROR = ''
+lexers.STYLE_FUNCTION = ''
+lexers.STYLE_HEADING = ''
+lexers.STYLE_KEYWORD = ''
+lexers.STYLE_LABEL = ''
+lexers.STYLE_NUMBER = ''
+lexers.STYLE_OPERATOR = ''
+lexers.STYLE_REGEX = ''
+lexers.STYLE_STRING = ''
+lexers.STYLE_PREPROCESSOR = ''
+lexers.STYLE_TAG = ''
+lexers.STYLE_TYPE = ''
+lexers.STYLE_VARIABLE = ''
+lexers.STYLE_WHITESPACE = ''
+lexers.STYLE_EMBEDDED = ''
+lexers.STYLE_IDENTIFIER = ''
+
+lexers.STYLE_LINENUMBER = ''
+lexers.STYLE_LINENUMBER_CURSOR = lexers.STYLE_LINENUMBER
+lexers.STYLE_CURSOR = 'back:white,fore:black'
+lexers.STYLE_CURSOR_PRIMARY = lexers.STYLE_CURSOR..',back:yellow'
+lexers.STYLE_CURSOR_LINE = ''
+lexers.STYLE_COLOR_COLUMN = 'back:red'
+lexers.STYLE_SELECTION = 'reverse'
+lexers.STYLE_STATUS = ''
+lexers.STYLE_STATUS_FOCUSED = 'reverse'
+lexers.STYLE_SEPARATOR = lexers.STYLE_DEFAULT
+lexers.STYLE_INFO = ''
+lexers.STYLE_EOF = ''
+
+-- Diff
+lexers.STYLE_ADDITION = 'fore:green'
+lexers.STYLE_DELETION = 'fore:red'
+lexers.STYLE_CHANGE = 'fore:yellow'
+
+-- Output
+lexers.STYE_FILENAME = ''
+lexers.STYLE_LINE = ''
+lexers.STYLE_COLUMN = ''
+lexers.STYLE_MESSAGE = ''
